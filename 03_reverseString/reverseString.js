bcff5bca4b35db6ca09063092e1750b24618ae85
boysetsfire <77065907+boysetsfire@users.noreply.github.com>
2023-05-30 22:42:09 +0200

    passed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringValue) {
+    let splitValue = stringValue.split('');
+    let reverseValue = splitValue.reverse();
+    let joinValue = reverseValue.join('');
+    return joinValue;
 };
 
 // Do not edit below this line
