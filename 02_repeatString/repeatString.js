b9b6b625ecb4a0a7c081c4682cc504a32480078f
boysetsfire <77065907+boysetsfire@users.noreply.github.com>
2023-05-30 22:29:35 +0200

    passed axercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let stringValue = '';
+    for (i = 0; i < num; i++) {
+        stringValue += string;
+    }
+    return stringValue
 };
 
 // Do not edit below this line
