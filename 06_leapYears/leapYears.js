d1477d5207e7706e6e5eac6114fcd8c5a6ff99cb
boysetsfire <77065907+boysetsfire@users.noreply.github.com>
2023-06-24 22:03:39 +0200

    passed 6 + 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,9 @@
 const leapYears = function(int) {
-    if (int % 4 === 0) {
+    if (int % 4 === 0 && (int % 400 === 0 || int % 100 !== 0)) {
         return true
     } else {
         return false
-    } 
+    }
 };
 
 // Do not edit below this line
